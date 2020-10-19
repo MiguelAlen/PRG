595a35855c52264f4823cf695efae9274aa76ad1
Miguel Alentado Garcia <miguel.kst@gmail.com>
2020-10-19 18:20:19 +0200

    Probando en casa

---
+++ T1/ej3.3.1.3/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ej3._3._1._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
