fb52211dcbd26e4d2d93084b4092c3d094f67e7f
miguel alentado garcia <miguel.kst@gmail.com>
2020-10-26 12:10:26 +0100

    ejercicios 26/10

---
+++ T1/ej.4.1.3.11/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ej._4._1._3._11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Introduce 4 numeros");
+            int[] numeros = new int[4];
+            int maximo = numeros[0];
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine("Introduce el numero {0}:", i + 1);
+                numeros[i] = Convert.ToInt32(Console.ReadLine());
+                if (maximo < numeros[i]) { maximo = numeros[i]; }
+            }
+            Console.WriteLine("El numero {0} es el valor mas alto.", maximo);
+        }   
+    }
+}
