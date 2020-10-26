f50b149aefcf0ea8a7edf0f3dd6cc0865904c397
miguel alentado garcia <miguel.kst@gmail.com>
2020-10-26 12:47:05 +0100

    ultimos ejercicios 4.1.3

---
+++ T1/ej4.1.3.10/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ej4._1._3._10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] vectores = new int[2, 3];
+            for(int i = 0; i < 2; i++) 
+            {
+                Console.WriteLine("Introduce el vector {0}:", i+1);
+                for (int j=0; j <3; j++)
+                {
+                    Console.WriteLine("Componente {0}:", j + 1);
+                    vectores[i, j] = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+            int pEscalar = (vectores[0, 0] * vectores[1, 0] +
+                            vectores[0, 1] * vectores[1, 1] +
+                            vectores[0, 2] * vectores[1, 2]);
+            Console.WriteLine("El prosucto escalar de ambos vectores es: {0}", pEscalar);
+        }
+    }
+}
--- T1/ej4.1.3.11/Program.cs
+++ T1/ej4.1.3.11/Program.cs
@@ -9,7 +9,7 @@ namespace ej._4._1._3._11
             
             Console.WriteLine("Introduce 4 numeros");
             int[] numeros = new int[4];
-            int maximo = numeros[0];
+            int maximo = Int32.MinValue;// tmb se puede con Int32.MaxValue
             for (int i = 0; i < 4; i++)
             {
                 Console.WriteLine("Introduce el numero {0}:", i + 1);
