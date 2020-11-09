02caf1eb568381d4f9a5296a59b82dcd1c02874a
miguel alentado garcia <miguel.kst@gmail.com>
2020-11-09 13:54:28 +0100

    Haciendo el perro

--- T1/ej4.3.1.1/Program.cs
+++ T1/ej4.3.1.1/Program.cs
@@ -11,15 +11,46 @@ namespace ej4._3._1._1
             public int pesoKb;
             public float duracion;
         }
+
         public static void Main()
         {
-            mp3 cancion;
-            cancion.artista = "art";
-            cancion.titulo = "tit";
-            cancion.pesoKb = 222;
-            cancion.duracion = 1.2f;
-            Console.WriteLine("La cancion de {0} es {1}, dura {2} minutos y pesa {3}KB."
-                ,cancion.artista, cancion.titulo, cancion.duracion, cancion.pesoKb);
+            mp3[] album = new mp3[100];
+            int opcion;
+            do
+            {
+                // Menu principal, repetitivo
+                Console.WriteLine();
+                Console.WriteLine("Lista de reproducción.\nEscoja una opción:");
+                Console.WriteLine("1.- Añadir nueva cancion");
+                Console.WriteLine("2.- Mostrar los nombres de todos las canciones");
+                Console.WriteLine("3.- Buscar cancion");
+                Console.WriteLine("4.- Cerrar lista de reproducción");
+                opcion = Convert.ToInt32(Console.ReadLine());
+
+                switch (opcion)
+                {
+
+                    case 1: // Añadir una nueva canción
+                        Console.WriteLine("Que canción desea añadir?");
+                        Console.WriteLine("A que cantante pertenece?");
+                        Console.WriteLine("Duracion de la canción?");
+                        Console.WriteLine("Peso (en KB) de la canción?");
+                        break;
+
+                    case 2: // Mostrar todos los titulos
+                        Console.WriteLine("");
+                        break;
+
+                    case 3: // Buscar cancion
+                        Console.WriteLine("");
+                        break;
+
+                    default: // Otra opcion: no válida
+                        Console.WriteLine("Opción desconocida!");
+                        break;
+                }
+            } while (opcion != 4);
+
         }
     }
 }
