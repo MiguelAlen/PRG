6ac1287c12625486e09ca977e0153e6283fbc3d2
miguel alentado garcia <miguel.kst@gmail.com>
2020-11-12 10:11:39 +0100

    Prg bases de datos

--- T1/ej4.3.1.1/Program.cs
+++ T1/ej4.3.1.1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace ej4._3._1._1
 {
@@ -9,13 +10,14 @@ namespace ej4._3._1._1
             public string artista;
             public string titulo;
             public int pesoKb;
-            public float duracion;
+            public double duracion;
         }
 
         public static void Main()
         {
             mp3[] album = new mp3[100];
             int opcion;
+            int posicion = 0;
             do
             {
                 // Menu principal, repetitivo
@@ -32,17 +34,33 @@ namespace ej4._3._1._1
 
                     case 1: // Añadir una nueva canción
                         Console.WriteLine("Que canción desea añadir?");
+                        album[posicion].titulo = Convert.ToString(Console.ReadLine());
                         Console.WriteLine("A que cantante pertenece?");
+                        album[posicion].artista = Convert.ToString(Console.ReadLine());
                         Console.WriteLine("Duracion de la canción?");
+                        album[posicion].duracion = Convert.ToDouble(Console.ReadLine());
                         Console.WriteLine("Peso (en KB) de la canción?");
+                        album[posicion].pesoKb = Convert.ToInt32(Console.ReadLine());
+                        posicion++;
                         break;
 
                     case 2: // Mostrar todos los titulos
-                        Console.WriteLine("");
+                        for(int i = 0; i < posicion; i++)
+                        {
+                            Console.WriteLine("{0}", album[i].titulo);
+                        }
                         break;
 
                     case 3: // Buscar cancion
-                        Console.WriteLine("");
+                        Console.WriteLine("Nombre de la canción?");
+                        string buscar = Convert.ToString(Console.ReadLine());
+                        bool encontrado = false;
+                        for (int i = 0; i < posicion; i++)
+                        {
+                            if (buscar == album[i].titulo) { encontrado = true; }
+                        }
+                        if (encontrado==true) { Console.WriteLine("La cancion buscada es la numero {0}", posicion + 1); }
+                        else {Console.WriteLine("La canción no está en el album"); }
                         break;
 
                     default: // Otra opcion: no válida
