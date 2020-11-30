d62b2b919c39085401fb8afd67ac34295dfb19fe
miguel alentado garcia <miguel.kst@gmail.com>
2020-11-30 13:52:23 +0100

    Ahorcado casi acabado

--- T1/ahorcado/Program.cs
+++ T1/ahorcado/Program.cs
@@ -11,11 +11,19 @@ namespace ahorcado
     {
         static void Main(string[] args)
         {
-            char[] palabra1 = {'p', 'r', 'o', 'g', 'r', 'a', 'm', 'a', 'c', 'i', 'o', 'n'};
-            char[] palabra2 = {'_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_', '_'};
+            // Palabra del ahorcado puesta en array, y array de '_' con tamaño de la palabra.
+            string cadena = Convert.ToString(Console.ReadLine());
+            char[] palabra1 = cadena.ToCharArray();
+            char[] palabra2 = new char[cadena.Length];
+            for (int i = 0; i < cadena.Length; i++)
+            {
+                palabra2[i] = '_';
+            }
+            // Variables locales
             int errores = 0;
-            string str2 = "";
+            string intento = "";
             char letra;
+            // Matriz de espacios para el dibujo
             char[,] dibujo = new char[6, 8];
             for (int i = 0; i < 6; i++)
             {
@@ -31,23 +39,23 @@ namespace ahorcado
             dibujo[5, 1] = '─'; dibujo[5, 2] = '─'; dibujo[5, 3] = '─'; dibujo[5, 4] = '─';
             dibujo[5, 5] = '─'; dibujo[5, 6] = '─'; dibujo[5, 7] = '─';
             dibujo[1, 0] = '│'; dibujo[2, 0] = '│'; dibujo[3, 0] = '│'; dibujo[4, 0] = '│';
-            Console.WriteLine("Cadena: programación");
+            Console.WriteLine("Cadena: {0}", cadena);
             //6 intentos
             while (errores < 6)
             {
                 try
                 {
-
-                    //acierto
-                    str2 = new string(palabra2);
-                    if (str2 == "programacion") { break; }
-                    //errores
+                    //array2 de '_' a string
+                    intento = new string(palabra2);
+                    //acierto todas las letras correctas
+                    if (intento == cadena) { break; }
+                    //intentos fallidos
                     if (errores == 1) { dibujo[1, 4] = 'o'; }
                     if (errores == 2) { dibujo[2, 3] = '/'; }
                     if (errores == 3) { dibujo[2, 4] = '|'; }
                     if (errores == 4) { dibujo[2, 5] = '\\'; }
                     if (errores == 5) { dibujo[3, 3] = '/'; }
-                    //mostrar dibujo
+                    //mostrar dibujo en relacion a intentos fallidos
                     if (mostrar == true)
                     {
                         for (int i = 0; i < 6; i++)
@@ -59,10 +67,16 @@ namespace ahorcado
                             Console.WriteLine();
                         }
                     }
-                    //buscar letra
+                    // variable para letras repetidas
                     int contador = 0;
-                    Console.WriteLine("Letra:");
+                    //introducir y buscar letra. Si no se introduce un char letra, pedir de nuevo
+                    Console.WriteLine("Introduce una letra:");
                     letra = Convert.ToChar(Console.ReadLine());
+                    while (bool IsLetter(letra) == false)
+                    {
+                        Console.WriteLine("Caracter no valido. Introduce una letra:");
+                        letra = Convert.ToChar(Console.ReadLine());
+                    }
                     for (int i = 0; i < palabra1.Length; i++)
                     {
                         if (palabra1[i] == letra)
@@ -86,13 +100,19 @@ namespace ahorcado
                     }
                     Console.WriteLine("");
                 }
+                //si no se introduce una letra, avisar y pedirla de nuevo
                 catch (Exception errorEncontrado)
                 {
-                    Console.WriteLine("Introduce una letra valida",
+                    Console.WriteLine("Caracter no valido. Introduce una letra:",
                     errorEncontrado.Message);
                 }
             }
-            if (errores == 6)
+            //
+            if (intento == cadena)
+            {
+                Console.WriteLine("Enhorabuena, has ganado!!!");
+            }
+            else
             {
                 dibujo[3, 5] = '\\';
                 for (int i = 0; i < 6; i++)
@@ -103,11 +123,7 @@ namespace ahorcado
                     }
                     Console.WriteLine();
                 }
-                Console.WriteLine("R.I.P.La palabra era programación.");
-            }
-            if (str2 == "programacion")
-            {
-                Console.WriteLine("Enhorabuena, has ganado!!!");
+                Console.WriteLine("R.I.P.La palabra era {0}.", cadena);
             }
         }
     }
