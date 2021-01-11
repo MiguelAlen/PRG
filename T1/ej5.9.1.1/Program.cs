9993792a97217c9f3b5e1788e0c11d317ae6fb18
miguel alentado garcia <miguel.kst@gmail.com>
2021-01-11 10:18:03 +0100

    ej 5.9 11/01

---
+++ T1/ej5.9.1.1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ej5._9._1._1
+{
+    class Program
+    {
+        public static void Main()
+        {
+            Random r = new Random();
+            int aleatorio = r.Next(1, 7);
+            Console.WriteLine("Tiras el dado (d6): {0}", aleatorio);
+        }
+
+    }
+}
---
+++ T1/ej5.9.1.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ej5._9._1._2
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int intento = 0;
+            //int intentos = 5;
+            Random r = new Random();
+            int aleatorio = r.Next(1, 101);
+            Console.WriteLine("Intenta acertar un número entre el 1 y el 100. Tienes 6 intentos.");
+            for (int i = 0; i < 6; i++) 
+            {
+                intento = Convert.ToInt32(Console.ReadLine());
+                if (aleatorio == intento) { Console.WriteLine("Has ganado."); break; }
+            }
+            if (aleatorio != intento) { Console.WriteLine("Has perdido."); }
+
+            /*while (true) 
+             {
+                 intento = Convert.ToInt32(Console.ReadLine());
+                 if (intentos == 0) { Console.WriteLine("Has perdido."); break; }
+                 if (aleatorio == intento) { Console.WriteLine("Has ganado."); break; }
+                 else { intentos--; }
+             }*/
+        }
+
+    }
+}
