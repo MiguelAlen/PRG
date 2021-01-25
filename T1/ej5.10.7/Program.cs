ff90d1af6322918c527024f957cf1e77a431a3e7
miguel alentado garcia <miguel.kst@gmail.com>
2021-01-25 14:01:25 +0100

    5.10

---
+++ T1/ej5.10.7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ej5._10._7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = string.Empty;
+            Console.WriteLine("Introduce una palabra:");
+            string s = Console.ReadLine();
+            int i = s.Length;
+            for (int j = i - 1; j >= 0; j--)
+            {
+                str = str + s[j];
+            }
+            if (str == s)
+            {
+                Console.WriteLine(s + "es palindrome");
+            }
+            else
+            {
+                Console.WriteLine(s + "no es palindrome");
+            }
+        }
+    }
+}
---
+++ T1/ej5.10.8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ej5._10._8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce dos números, A y B");
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int r = 1, mcd =1;
+            while (r != 0) 
+            {
+                mcd = B;
+                r = A % B;
+                A = B;
+                B = r;
+            }
+            Console.WriteLine("El mcd és {0}.", mcd);
+        }
+    }
+}
