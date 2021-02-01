88caa9021e371b45e96478e30482745c0739640d
miguel alentado garcia <miguel.kst@gmail.com>
2021-02-01 13:58:48 +0100

    5.10

---
+++ T1/ej5.10.9/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ej5._10._9
+{
+    class Program
+    {
+        public static int subcadenaEn(string cadena, string subCadena)
+        {
+            if (cadena.Contains(subCadena))
+            { 
+                
+            };
+            return 0;
+        }
+        public static void Main()
+        {
+            Console.WriteLine("Introduce una cadena:");
+            string cadena = Convert.ToString(Console.ReadLine());
+            Console.WriteLine("Introduce una subcadena");
+            string subCadena = Convert.ToString(Console.ReadLine());
+           
+            
+        }
+    }
+}
