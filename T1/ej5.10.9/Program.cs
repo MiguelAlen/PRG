af834dcb77b5d4049f6c279937eca0a377559c1b
miguel alentado garcia <miguel.kst@gmail.com>
2021-02-04 11:48:53 +0100

    04/02

--- T1/ej5.10.9/Program.cs
+++ T1/ej5.10.9/Program.cs
@@ -6,11 +6,27 @@ namespace ej5._10._9
     {
         public static int subcadenaEn(string cadena, string subCadena)
         {
-            if (cadena.Contains(subCadena))
-            { 
-                
-            };
-            return 0;
+            if (subCadena.Length > cadena.Length) { return 0; }
+            string s = "";
+            int contador = 0;
+            if (cadena.Contains(subCadena)) { contador++; }
+            else {
+                for (int i = 0; i < cadena.Length; i++)
+                {
+                    for (int j = 0; j < subCadena.Length; j++)
+                    {
+                        if (cadena[i] == subCadena[j]) { s += cadena[i]; }
+                    }
+                }
+                for (int i = 0; i < s.Length - 1; i++)
+                {
+                    if (s[i] == s[i + 1])
+                    {
+                        contador += 2;
+                    }
+                }
+            }
+            return contador;
         }
         public static void Main()
         {
@@ -18,8 +34,7 @@ namespace ej5._10._9
             string cadena = Convert.ToString(Console.ReadLine());
             Console.WriteLine("Introduce una subcadena");
             string subCadena = Convert.ToString(Console.ReadLine());
-           
-            
+            Console.WriteLine("{0}", subcadenaEn(cadena, subCadena));
         }
     }
 }
