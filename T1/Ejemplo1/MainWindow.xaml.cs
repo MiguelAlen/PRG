8314834fff3299a5dd4dc246de1e5732a239a749
Miguel Alentado Garcia <miguel.kst@gmail.com>
2021-02-19 23:39:01 +0100

    p3done

--- T1/Ejemplo1/MainWindow.xaml.cs
+++ T1/Ejemplo1/MainWindow.xaml.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿// --------------------------------------------
+// Miguel
+// Curso DAM
+// Modalidad Presencial
+// Práctica nº 3
+// --------------------------------------------
+
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,6 +21,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+
 
 
 
@@ -25,6 +36,7 @@ namespace Ejemplo1
         public MainWindow()
         {
             InitializeComponent();
+            
         }
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
@@ -37,52 +49,14 @@ namespace Ejemplo1
                 e.Cancel = true;
             }
         }
-
+            
         private void BtnSalir_Click(object sender, RoutedEventArgs e)
         {
-            //if(cbEstudias.IsChecked == true)
             Close();
         }
-        /*private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-
-        }
-
-        private void cbProvincias_DropDownClosed(object sender, EventArgs e)
-        {
-            MessageBox.Show(cbProvincias.Text +
-                            " - SelectedIndex: "
-                             + Convert.ToString(cbProvincias.SelectedIndex));
-        }
-
-        private void btProvincias_Click(object sender, RoutedEventArgs e)
-        {
-            if(cbProvincias.SelectedIndex == -1)
-            {
-                MessageBox.Show(cbProvincias.Text);
-            }
-        }*/
-
-        private void rdVehiculoSI_Checked(object sender, RoutedEventArgs e)
-        {
-            cbTipoVehiculo.IsEnabled = true;
-        }
-
-        private void rdVehiculoNo_Checked(object sender, RoutedEventArgs e)
-        {
-            cbTipoVehiculo.SelectedIndex = -1;
-            cbTipoVehiculo.IsEnabled = false;
-        }
-
-        private void txtNombre_GotFocus(object sender, RoutedEventArgs e)
-        {
-           
-        }
-
         private void txtNombre_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
-
+           
             if (txtNombre.Text != "") 
             {
                 txtNombre.Background = Brushes.White; 
@@ -97,6 +71,7 @@ namespace Ejemplo1
 
         private void txtApellido1_TextChanged(object sender, TextChangedEventArgs e)
         {
+            
             if (txtApellido1.Text != "")
             {
                 txtApellido1.Background = Brushes.White;
@@ -111,6 +86,7 @@ namespace Ejemplo1
 
         private void txtTelefono1_TextChanged(object sender, TextChangedEventArgs e)
         {
+           
             if (txtTelefono1.Text != "")
             {
                 txtTelefono1.Background = Brushes.White;
@@ -121,19 +97,19 @@ namespace Ejemplo1
                 var brush = (Brush)converter.ConvertFromString("#FFE68080");
                 txtTelefono1.Background = brush;
             }
-            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtTelefono1.Text, "[^0-9]"))
             {
-                MessageBox.Show("Please enter only numbers.");
-                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                MessageBox.Show("Introduzca numeros unicamente.");
+                txtTelefono1.Text = txtTelefono1.Text.Remove(txtTelefono1.Text.Length - 1);
             }
         }
 
         private void txtTelefono2_TextChanged(object sender, TextChangedEventArgs e)
         {
-            if (System.Text.RegularExpressions.Regex.IsMatch(textBox1.Text, "[^0-9]"))
+            if (System.Text.RegularExpressions.Regex.IsMatch(txtTelefono2.Text, "[^0-9]"))
             {
-                MessageBox.Show("Please enter only numbers.");
-                textBox1.Text = textBox1.Text.Remove(textBox1.Text.Length - 1);
+                MessageBox.Show("Introduzca numeros unicamente.");
+                txtTelefono2.Text = txtTelefono2.Text.Remove(txtTelefono2.Text.Length - 1);
             }
             if (txtTelefono2.Text != "")
             {
@@ -142,9 +118,131 @@ namespace Ejemplo1
             else
             {
                 var converter = new System.Windows.Media.BrushConverter();
-                var brush = (Brush)converter.ConvertFromString("#FFE68080");
+                var brush = (Brush)converter.ConvertFromString("#FFB7E4E2");
                 txtTelefono2.Background = brush;
             }
+        }
+
+        private void txtPoblacion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+            if (txtPoblacion.Text != "")
+            {
+                txtPoblacion.Background = Brushes.White;
+            }
+            else
+            {
+                var converter = new System.Windows.Media.BrushConverter();
+                var brush = (Brush)converter.ConvertFromString("#FFE68080");
+                txtPoblacion.Background = brush;
+            }
+        }
+
+        private void rdCliente_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            cbProvincia.IsEnabled = true;
+            cbProvincia.Items.Clear();
+            cbProvincia.Items.Add("Castellón");
+            cbProvincia.Items.Add("Valencia");
+            cbProvincia.Items.Add("Alicante");
+            cbProvincia.Items.Add("Murcia");
+            cbProvincia.Items.Add("Albacete");
+            cbProvincia.Items.Add("Cuenca");
+            cbProvincia.Items.Add("Teruel");
+        }
+
+        private void rdDistribuidor_Checked(object sender, RoutedEventArgs e)
+        {
+            
+            cbProvincia.IsEnabled = true;
+            cbProvincia.Items.Clear();
+            cbProvincia.Items.Add("Castellón");
+            cbProvincia.Items.Add("Valencia");
+            cbProvincia.Items.Add("Alicante");
+        }
+
+        private void txtCorreo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            if (txtCorreo.Text != "")
+            {
+                txtCorreo.Background = Brushes.White;
+            }
+            else
+            {
+                var converter = new System.Windows.Media.BrushConverter();
+                var brush = (Brush)converter.ConvertFromString("#FFE68080");
+                txtCorreo.Background = brush;
+            }
+        }
+
+        private void txtApellido2_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtApellido2.Text != "")
+            {
+                txtApellido2.Background = Brushes.White;
+            }
+            else
+            {
+                var converter = new System.Windows.Media.BrushConverter();
+                var brush = (Brush)converter.ConvertFromString("#FFB7E4E2");
+                txtApellido2.Background = brush;
+            }
+        }
+
+        private void txtDireccion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            if (txtDireccion.Text != "")
+            {
+                txtDireccion.Background = Brushes.White;
+            }
+            else
+            {
+                var converter = new System.Windows.Media.BrushConverter();
+                var brush = (Brush)converter.ConvertFromString("#FFE68080");
+                txtDireccion.Background = brush;
+            }
+        }
+
+        private void txtCp_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+            if (txtCp.Text != "")
+            {
+                txtCp.Background = Brushes.White;
+            }
+            else
+            {
+                var converter = new System.Windows.Media.BrushConverter();
+                var brush = (Brush)converter.ConvertFromString("#FFE68080");
+                txtCp.Background = brush;
+            }
+        }
+
+        private void BtnEnviar_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("");
+            /*
+            if (rdCliente.IsChecked) 
+            { 
+                MessageBox.Show("INSERT INTO clientes(....) VALUES(...)"); 
+            }
+            else
+            {
+                MessageBox.Show("INSERT INTO distribuidores(...) VALUES(...)");
+            }
+            */
+
+        }
+
+        private void BtnEnviar_LayoutUpdated(object sender, EventArgs e)
+        {
+            BtnEnviar.IsEnabled = !string.IsNullOrEmpty(txtNombre.Text) && !string.IsNullOrEmpty(txtApellido1.Text)
+                && !string.IsNullOrEmpty(txtTelefono1.Text) && !string.IsNullOrEmpty(txtPoblacion.Text)
+                && !string.IsNullOrEmpty(txtCorreo.Text) && !string.IsNullOrEmpty(txtDireccion.Text)
+                && !string.IsNullOrEmpty(txtCp.Text) && !string.IsNullOrEmpty(cbProvincia.Text);
         }
 
     }   
