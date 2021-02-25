c2d9140e6951bb22966dbabfd580cb60f2478c5e
miguel alentado garcia <miguel.kst@gmail.com>
2021-02-25 10:51:34 +0100

    Editor

--- T1/Q3Editor/MainWindow.xaml.cs
+++ T1/Q3Editor/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.Win32;
+using System.IO;
 
 namespace Q3Editor
 {
@@ -20,35 +22,161 @@ namespace Q3Editor
     /// </summary>
     public partial class MainWindow : Window
     {
-        private bool guardado = false;
+        private bool guardado = true;
+        private MessageBoxResult ComfirmacionBox(string texto)
+        {
+            return MessageBox.Show(texto, "Confirmación", MessageBoxButton.YesNo, MessageBoxImage.Question);
+        }
+        private void SeGuarda()
+        {
+            if(txtTexto.Text != "" && !guardado)
+            {
+                if(ComfirmacionBox("¿Desea guardar el texto?") == MessageBoxResult.Yes)
+                {
+                    Guardar();
+                }
+            }
+        }
+
         public MainWindow()
         {
             InitializeComponent();
+            guardado = true;
+            RoutedCommand Ctrl_N = new RoutedCommand();
+            Ctrl_N.InputGestures.Add(new KeyGesture(Key.N, ModifierKeys.Control));
+            CommandBindings.Add(new CommandBinding(Ctrl_N, mArchivoNuevo_Click));
+            
+            RoutedCommand Ctrl_O = new RoutedCommand();
+            Ctrl_O.InputGestures.Add(new KeyGesture(Key.O, ModifierKeys.Control));
+            CommandBindings.Add(new CommandBinding(Ctrl_O, mArchivoNuevo_Click));
+
+            RoutedCommand Ctrl_S = new RoutedCommand();
+            Ctrl_S.InputGestures.Add(new KeyGesture(Key.S, ModifierKeys.Control));
+            CommandBindings.Add(new CommandBinding(Ctrl_S, mArchivoNuevo_Click));
+
+            RoutedCommand Alt_F4 = new RoutedCommand();
+            Alt_F4.InputGestures.Add(new KeyGesture(Key.F4, ModifierKeys.Control));
+            CommandBindings.Add(new CommandBinding(Alt_F4, mArchivoNuevo_Click));
+
+            txtTexto.Focus();
         }
 
         private void mArchivoSalir_Click(object sender, RoutedEventArgs e)
         {
+            GuardarComo();
             Close();
         }
 
         private void mArchivoNuevo_Click(object sender, RoutedEventArgs e)
         {
             Guardar();
-            txtMain.Clear();
+            txtTexto.Clear();
+        }
+        
+        private void mArchivoAbrir_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            var path = System.Environment.GetFolderPath(System.Environment.SpecialFolder.MyDocuments);
+
+            openFileDialog.InitialDirectory = path;
+            openFileDialog.Filter = "txt files(*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog.FilterIndex = 1;
+            openFileDialog.RestoreDirectory = true;
+
+            // Si pulsamos abrir... del menu abrir
+            if (openFileDialog.ShowDialog() == true)
+            {
+                // Declaro una variable de tipo fichero
+                var fileStream = openFileDialog.OpenFile();
+
+                // Creo un canal de lectura (Objeto StreamReader Instanciado)
+                using (StreamReader reader = new StreamReader(fileStream))
+                {
+                    // Rellenamos el TextBox con el contenido del fichero
+                    txtTexto.Text = reader.ReadToEnd();
+                }
+                //Alternativa simple.
+                //txtTexto.Text = File.ReadAllText(openFileDialog.FileName);
+
+                // En la barra de estado pongo el nombre del fichero
+                panelNombre.Content = openFileDialog.FileName;
+
+                // Y indico que esta guardado
+                guardado = true;
+                panelEstado.Content = "Guardado";
+            }
         }
         private void Guardar()
         {
-            if (txtMain.Text != "" && !guardado)
+            // Si no esta guardado...
+            if (!guardado)
             {
-                if (MessageBox.Show("¿Quiere guardar el contenido?", 
-                                                    "Confirmación.",
-                                                    MessageBoxButton.YesNo,
-                                                    MessageBoxImage.Question)
-                        == MessageBoxResult.Yes)
+                // Si no tiene nombre
+                if (panelNombre.Content.ToString() == "Sin Nombre")
                 {
-                    MessageBox.Show("Guardando...");
-                    guardado = true;
+                    GuardarComo();
                 }
+                else
+                {
+                    // Lo guardamos en el fichero
+                    File.WriteAllText(panelNombre.Content.ToString(), txtTexto.Text);
+
+                    // Se ha gurdado el texto
+                    guardado = true;
+
+                    // Indicamos que se ha guardado
+                    panelEstado.Content = "Guardado";
+                }
+            }
+        }
+
+        private void mArchivoGuardar_Click(object sender, RoutedEventArgs e)
+        {
+            Guardar();
+        }
+        private void GuardarComo()
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+
+            saveFileDialog.InitialDirectory = "D:\\";
+            saveFileDialog.Filter = "Archivos de texto (*.txt)|*.txt|Todos los archivos (*.*)|*.*";
+            saveFileDialog.FilterIndex = 1;
+            saveFileDialog.RestoreDirectory = false;
+
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                // Indicamos el nombre
+                panelNombre.Content = saveFileDialog.FileName;
+
+                // Lo guardamos en el fichero
+                File.WriteAllText(panelNombre.Content.ToString(), txtTexto.Text);
+
+                // Se ha guardado el texto
+                guardado = true;
+
+                // Indicamos que se ha guardado
+                panelEstado.Content = "Guardado";
+            }
+        }
+        private void mArchivoGuardarComo_Click(object sender, RoutedEventArgs e)
+        {
+            GuardarComo();   
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SeGuarda();
+        }
+
+        private void txtTexto_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(guardado == true)
+            {
+                // Cada vez que cambie el contenido del texto indicaremos que hay que guardarlo
+                guardado = false;
+
+                panelEstado.Content = "No Guardado";
             }
         }
     }
