74fe53c1c42300c98dc6e7447669c7b0f675879e
miguel alentado garcia <miguel.kst@gmail.com>
2021-03-04 11:08:31 +0100

    Ejercicios 04/03

--- T1/ej6.2.2/CJuego.cs
+++ T1/ej6.2.2/CJuego.cs
@@ -9,9 +9,17 @@ namespace ej6._2._2
         {
             Console.WriteLine(CBienvenida.BIENVENIDA);
         }
+        public void Partida()
+        {
+            Console.WriteLine(CPartida.PARTIDA);
+        }
         static void Main(string[] args)
         {
-           
+            Juego j = new Juego();
+            j.Lanzar();
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
+            j.Partida();
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
         }
     }
 }
--- T1/ej6.2.2/CPartida.cs
+++ T1/ej6.2.2/CPartida.cs
@@ -6,5 +6,6 @@ namespace ej6._2._2
 {
     class CPartida
     {
+        public const string PARTIDA = "Ésta sería la pantalla de juego.Pulse Intro para salir";
     }
 }
