7d4e0e21581e1e2399ddc800ad08b53f582175d5
miguel alentado garcia <miguel.kst@gmail.com>
2021-03-15 13:55:42 +0100

    15/03

---
+++ T1/ej7.6.1/Matriz.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ej7._6._1
+{
+    class Matriz
+    {
+        int orden;
+        int[,] matriz;
+        public Matriz(int orden)
+        {
+            this.orden = orden;
+            this.matriz = new int[orden, orden];
+        }
+        
+
+    }
+}
