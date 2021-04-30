555b125b1d1efdc7916c6fb3c6a50fde7802eaa8
miguel alentado garcia <miguel.kst@gmail.com>
2021-04-30 13:56:17 +0200

    p4

--- T1/practica4/Program.cs
+++ T1/practica4/Program.cs
@@ -16,40 +16,61 @@ namespace practica4
 
             string path = Convert.ToString(Console.ReadLine());
 
-            try 
-            { 
-                fichero = File.OpenText(path);
-                do
+            string valencia = @"D:\Alentado\PRG\ficheros\valencia.txt";
+            string alicante = @"D:\Alentado\PRG\ficheros\alicante.txt";
+            string castellon = @"D:\Alentado\PRG\ficheros\castellon.txt";
+
+            using (StreamWriter a = File.CreateText(alicante))
+            using (StreamWriter v = File.CreateText(valencia))
+            using (StreamWriter c = File.CreateText(castellon))
+
+                if (!File.Exists(path))
                 {
-                    linea = fichero.ReadLine();
-                    if (linea != null)
+                    try
                     {
-                        //ALICANTE
-                        if (linea.Contains("Alicante") == true)
+                        fichero = File.OpenText(path);
+                        do
                         {
+                            linea = fichero.ReadLine();
+                            if (linea != null)
+                            {
+                                Console.WriteLine(linea);
+                                //ALICANTE
+                                if (linea.Contains("Alicante") == true)
+                                {
+                                    using (StreamWriter al = File.AppendText(alicante))
+                                    {
+                                        al.WriteLine(linea);
+                                    }
+                                }
 
+                                //VALENCIA
+                                if (linea.Contains("Valencia") == true)
+                                {
+                                    using (StreamWriter val = File.AppendText(valencia))
+                                    {
+                                        val.WriteLine(linea);
+                                    }
+                                }
+
+                                //CASTELLON
+                                if (linea.Contains("Castellón") == true)
+                                {
+                                    using (StreamWriter cast = File.AppendText(castellon))
+                                    {
+                                        cast.WriteLine(linea);
+                                    }
+                                }
+                            }
                         }
-
-                        //VALENCIA
-                        if (linea.Contains("Valencia") == true)
-                        {
-
-                        }
-
-                        //CASTELLON
-                        if (linea.Contains("Castellón") == true)
-                        {
-
-                        }
-                    }                   
+                        while (linea != null);
+                        fichero.Close();
+                    }
+                    catch (Exception e)
+                    {
+                        Console.WriteLine("Ha ocurrido un error inesperado {0}", e);
+                    }
                 }
-                while (linea != null);
-                fichero.Close();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine("Ha ocurrido un error inesperado {0}", e);
-            }
         }
     }
 }
